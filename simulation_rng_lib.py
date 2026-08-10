d75f0144f29e8f76fc4f075868e7a1ee764fd664
Nicks24 <141518296+Nixed24@users.noreply.github.com>
2026-08-10 02:12:13 +0100

    Add files via upload

---
+++ simulation_rng_lib.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 10 02:07:17 2026
+
+Simulation system for particles moving through media configurations created in
+Hammer
+
+@author: sainttux
+"""
+
